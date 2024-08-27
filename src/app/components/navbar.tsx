29a9d813fa5aca6276f1f370d96c164402653991
aman-bhatti <ai.bhatti13@gmail.com>
2024-08-27 19:40:25 -0400

    added navbar highlight

--- src/app/components/navbar.tsx
+++ src/app/components/navbar.tsx
@@ -1,4 +1,7 @@
+// src/app/components/navbar.tsx
+
 import Link from "next/link";
+import { usePathname } from "next/navigation"; // Import usePathname
 import { ModeToggle } from "./theme-toggle";
 import "../styles/navbar.css";
 
@@ -24,6 +27,8 @@ const navItems: Record<string, NavItem> = {
 };
 
 export function Navbar() {
+  const currentPath = usePathname(); // Use usePathname hook
+
   return (
     <aside className="-ml-[8px] mb-12 mt-10 tracking-tight">
       <div className="lg:sticky lg:top-20">
@@ -32,13 +37,20 @@ export function Navbar() {
           id="nav"
         >
           <Link
-            href={"/"}
-            className="italic pl-2 text-gray-700 dark:text-neutral-400 hover:text-neutral-600 focus:text-neutral-600 dark:hover:text-stone-500 dark:focus:text-stone-500"
+            href="/"
+            className={`italic pl-2 text-gray-700 dark:text-neutral-400 hover:text-neutral-600 focus:text-neutral-600 dark:hover:text-stone-500 dark:focus:text-stone-500 ${
+              currentPath === "/" ? "active-class" : ""
+            }`}
           >
             aman
           </Link>
           <div className="flex ms-auto flex-row space-x-0 pr-4">
             {Object.entries(navItems).map(([path, { name, external }]) => {
+              const isActive = currentPath === path; // Check if the current path matches
+              const linkClass = `text-gray-700 dark:text-neutral-400 transition-all hover:text-neutral-600 focus:text-neutral-600 dark:hover:text-stone-500 dark:focus:text-stone-500 flex align-middle relative py-1 px-2 ${
+                isActive ? "active-class" : ""
+              }`; // Apply active-class conditionally
+
               if (external) {
                 return (
                   <a
@@ -46,18 +58,14 @@ export function Navbar() {
                     href={path}
                     target="_blank"
                     rel="noopener noreferrer"
-                    className="text-gray-700 dark:text-neutral-400 transition-all hover:text-neutral-600 focus:text-neutral-600 dark:hover:text-stone-500 dark:focus:text-stone-500 flex align-middle relative py-1 px-2"
+                    className={linkClass}
                   >
                     {name}
                   </a>
                 );
               } else {
                 return (
-                  <Link
-                    key={path}
-                    href={path}
-                    className="text-gray-700 dark:text-neutral-400 transition-all hover:text-neutral-600 focus:text-neutral-600 dark:hover:text-stone-500 dark:focus:text-stone-500 flex align-middle relative py-1 px-2"
-                  >
+                  <Link key={path} href={path} className={linkClass}>
                     {name}
                   </Link>
                 );
